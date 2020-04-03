fc83f7b3da5102bae8022a5e982082de277dd85e
Arnaldo Alves Viana Junior <arnaldo_viana@yahoo.com.br>
2020-04-03 15:21:27 -0300

    atualizacao_script

--- garra/instala_garra.sh
+++ garra/instala_garra.sh
@@ -2,16 +2,16 @@
 #!/bin/bash
 echo ============================= RESOLVENDO CONFLITOS =============================
 
-sudo apt-get install ros-melodic-ros-controllers
-sudo apt-get install ros-melodic-gazebo*
-sudo apt-get install  ros-melodic-robotis-*
-sudo apt-get install ros-melodicc-gazebo-ros-control
-sudo apt-get install ros-melodic-diff-drive-controller
-sudo apt-get install ros-melodic-moveit
-sudo apt-get install ros-melodic-effort-controllers
-sudo apt-get install ros-melodic-joint-state-controller
-sudo apt-get install ros-melodic-position-controllers
-sudo apt update
+sudo apt-get install -y ros-melodic-ros-controllers
+sudo apt-get install -y ros-melodic-gazebo*
+sudo apt-get install -y ros-melodic-robotis-*
+sudo apt-get install -y ros-melodicc-gazebo-ros-control
+sudo apt-get install -y ros-melodic-diff-drive-controller
+sudo apt-get install -y ros-melodic-moveit
+sudo apt-get install -y ros-melodic-effort-controllers
+sudo apt-get install -y ros-melodic-joint-state-controller
+sudo apt-get install -y ros-melodic-position-controllers
+sudo apt update -y
 rm -rf ~/catkin_ws/src/turtlebot3_manipulation
 cd ~/catkin_ws/src
 
@@ -21,7 +21,6 @@ git clone https://github.com/ROBOTIS-GIT/turtlebot3_manipulation_simulations.git
 cd turtlebot3_manipulation_simulations
 git checkout melodic-devel
 cd ~/catkin_ws
-sudo apt install ros-melodic-moveit 
 catkin_make
 
 echo ============================ SO TESTAR JOVEM! ============================
