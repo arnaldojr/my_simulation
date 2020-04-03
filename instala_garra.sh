03827b052e13117591784f9be0211d9c701e8844
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-04-03 12:03:54 -0300

    Update instala_garra.sh

--- instala_garra.sh
+++ instala_garra.sh
@@ -1,7 +1,12 @@
 
 #!/bin/bash
 echo ============================= RESOLVENDO CONFLITOS =============================
-sudo apt-get install ros-melodicc-gazebo-ros-control ros-melodic-diff-drive-controller
+
+sudo apt-get install ros-melodic-ros-controllers
+sudo apt-get install ros-melodic-gazebo*
+sudo apt-get install  ros-melodic-robotis-*
+sudo apt-get install ros-melodicc-gazebo-ros-control
+sudo apt-get install ros-melodic-diff-drive-controller
 sudo apt-get install ros-melodic-moveit
 sudo apt-get install ros-melodic-effort-controllers
 sudo apt-get install ros-melodic-joint-state-controller
@@ -11,9 +16,7 @@ rm -rf ~/catkin_ws/src/turtlebot3_manipulation
 cd ~/catkin_ws/src
 
 echo ============================ CLONANDO REPOSITORIOS ============================
-git clone https://github.com/ROBOTIS-GIT/open_manipulator_with_tb3.git
-git clone https://github.com/ROBOTIS-GIT/open_manipulator_with_tb3_msgs.git
-git clone https://github.com/ROBOTIS-GIT/open_manipulator_perceptions.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_manipulation.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_manipulation_simulations.git
 cd turtlebot3_manipulation_simulations
 git checkout melodic-devel
