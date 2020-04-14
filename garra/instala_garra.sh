80d76e81f14692bfdf9b176304f58907ac086847
Licia Sales <40764768+liciascl@users.noreply.github.com>
2020-04-14 17:23:09 -0300

    Update instala_garra.sh

--- garra/instala_garra.sh
+++ garra/instala_garra.sh
@@ -27,5 +27,13 @@ git clone https://github.com/ROBOTIS-GIT/turtlebot3_manipulation_simulations.git
 cd ~/catkin_ws
 catkin_make
 
+
+echo excluindo os urdfs sem camera
+mv $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator_x.urdf.xacro $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator.urdf.xacro
+mv $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator_x.gazebo.xacro $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator.gazebo.xacro
+
+cp $HOME/catkin_ws/src/my_simulation/models/garra/* $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf
+
+
 echo ============================ SO TESTAR JOVEM! ============================
 
