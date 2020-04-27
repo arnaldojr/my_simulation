94d52f2ae0fb299a09125278c50cd92adb4bad45
mirwox <fmiranda@gmail.com>
2020-04-27 11:43:23 -0300

    atualizacao modelo sala 404

--- garra/instala_garra.sh
+++ garra/instala_garra.sh
@@ -11,9 +11,6 @@ sudo apt-get install -y ros-melodic-moveit*
 sudo apt-get install -y ros-melodic-effort-controllers
 sudo apt-get install -y ros-melodic-joint-state-controller
 sudo apt-get install -y ros-melodic-position-controllers
-sudo apt update -y
-sudo apt upgrade -y
-rm -rf ~/catkin_ws/src/turtlebot3_manipulation
 cd ~/catkin_ws/src
 
 echo ============================ CLONANDO REPOSITORIOS ============================
@@ -26,14 +23,3 @@ git clone https://github.com/ROBOTIS-GIT/turtlebot3_manipulation_simulations.git
 
 cd ~/catkin_ws
 catkin_make
-
-
-echo excluindo os urdfs sem camera
-mv $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator_x.urdf.xacro $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator.urdf.xacro
-mv $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator_x.gazebo.xacro $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf/open_manipulator.gazebo.xacro
-
-cp $HOME/catkin_ws/src/my_simulation/models/garra/* $HOME/catkin_ws/src/turtlebot3_manipulation/turtlebot3_manipulation_description/urdf
-
-
-echo ============================ SO TESTAR JOVEM! ============================
-
